cd3fd617be024eb78f77d57a65e2f78db57116b1
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-16 16:39:05 +0400

    update: re-init app with yarn

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import express from "express";
+import config from "config";
+
+const app = express();
+
+const port = config.get("port");
+
+app.listen(port, () => {
+  console.log(`App started at http:localhost:${port}`);
+});
