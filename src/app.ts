fe4b4c01d130a7e24243e1e7a461e9ae033b6a67
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-16 17:43:03 +0400

    update: routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express from "express";
 import config from "config";
 import connectToDb from "./utils/connectToDb";
 import log from "./utils/logger";
+import routes from "./routes";
 
 const app = express();
+
+app.use(routes);
 
 const port = config.get("port");
 
