111ba9d5b2f0361ab5ffff59cd2b07e25a0df7bc
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-17 17:27:07 +0400

    update: envs

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,11 @@ export default {
   port: process.env.PORT,
   dbUri: process.env.DB_URI,
   logLevel: "info",
+  smtp: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: process.env.SMTP_SECURE,
+  },
 };
