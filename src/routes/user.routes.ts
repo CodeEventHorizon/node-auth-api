cd43675495f71285eff97958c0d939c3a9586246
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-20 17:42:00 +0400

    add: user forgot password route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import validateResource from "../middleware/validateResource";
-import { createUserSchema, verifyUserSchema } from "../schema/user.schema";
+import {
+  createUserSchema,
+  forgotPasswordSchema,
+  verifyUserSchema,
+} from "../schema/user.schema";
 import {
   createUserHandler,
+  forgotPasswordHandler,
   verifyUserHandler,
 } from "../controller/user.controller";
 
@@ -18,6 +23,12 @@ router.post(
   "/api/users/verify/:id/:verificationCode",
   validateResource(verifyUserSchema),
   verifyUserHandler
+);
+
+router.post(
+  "/api/users/forgotpassword",
+  validateResource(forgotPasswordSchema),
+  forgotPasswordHandler
 );
 
 export default router;
