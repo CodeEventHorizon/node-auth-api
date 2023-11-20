6266906cb4c86d1679ef94d8bea2ab1095d46bfb
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-20 16:47:19 +0400

    update: default

--- config/default.ts
+++ config/default.ts
@@ -1,12 +1,12 @@
 export default {
-  port: process.env.PORT,
-  dbUri: process.env.DB_URI,
-  logLevel: "info",
+  port: parseInt(process.env.PORT || "3000") as number,
+  dbUri: process.env.DB_URI as string,
+  logLevel: "info" as const,
   smtp: {
     user: process.env.SMTP_USER as string,
     pass: process.env.SMTP_PASS as string,
     host: process.env.SMTP_HOST as string,
-    port: 587,
-    secure: false,
+    port: parseInt(process.env.SMTP_PORT || "587") as number,
+    secure: (process.env.SMTP_SECURE === "true") as boolean,
   },
 };
