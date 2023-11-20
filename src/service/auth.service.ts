5e39c9d6d3a7f366119e55f82d3379055e9f5c52
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-20 20:00:59 +0400

    update: expires and omit private fields

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,5 +1,6 @@
 import { DocumentType } from "@typegoose/typegoose";
-import { User } from "../model/user.model";
+import { omit } from "lodash";
+import { User, privateFields } from "../model/user.model";
 import { signJwt } from "../utils/jwt";
 import SessionModel from "../model/session.model";
 
@@ -7,20 +8,29 @@ export async function createSession({ userId }: { userId: string }) {
   return SessionModel.create({ user: userId });
 }
 
+export async function findSessionById(id: string) {
+  return SessionModel.findById(id);
+}
+
 export async function signRefreshToken({ userId }: { userId: string }) {
   const session = await createSession({ userId });
 
   const refreshToken = signJwt(
     { session: session._id },
-    "refreshTokenPrivateKey"
+    "refreshTokenPrivateKey",
+    {
+      expiresIn: "1y",
+    }
   );
 
   return refreshToken;
 }
 
 export function signAccessToken(user: DocumentType<User>) {
-  const payload = user.toJSON();
-  const accessToken = signJwt(payload, "accessTokenPrivateKey");
+  const payload = omit(user.toJSON(), privateFields);
+  const accessToken = signJwt(payload, "accessTokenPrivateKey", {
+    expiresIn: "15m",
+  });
 
   return accessToken;
 }
