a0fcc015c20322540860bbc2c1b1931d65a0322f
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-20 17:38:25 +0400

    add: forget password handler controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
-import { CreateUserInput, VerifyUserInput } from "../schema/user.schema";
-import { createUser, findUserById } from "../service/user.service";
+import {
+  CreateUserInput,
+  ForgotPasswordInput,
+  VerifyUserInput,
+} from "../schema/user.schema";
+import {
+  createUser,
+  findUserByEmail,
+  findUserById,
+} from "../service/user.service";
 import sendEmail from "../utils/mailer";
+import log from "../utils/logger";
+import { nanoid } from "nanoid";
 
 export async function createUserHandler(
   req: Request<{}, {}, CreateUserInput>,
@@ -58,4 +68,39 @@ export async function verifyUserHandler(
   }
 
   return res.send("Could not verify user");
+}
+
+export async function forgotPasswordHandler(
+  req: Request<{}, {}, ForgotPasswordInput>,
+  res: Response
+) {
+  const message =
+    "If a user with that email is registered you will receive a password reset email";
+  const { email } = req.body;
+  const user = await findUserByEmail(email);
+
+  if (!user) {
+    log.debug(`User with email ${email} does not exist`);
+    return res.send(message);
+  }
+
+  if (!user.verified) {
+    return res.send("User is not verified");
+  }
+
+  const passwordResetCode = nanoid();
+
+  user.passwordResetCode = passwordResetCode;
+
+  await user.save();
+
+  await sendEmail({
+    from: "test@example.com",
+    to: user.email,
+    subject: "Reset your password",
+    text: `Password reset code: ${passwordResetCode}. Id ${user._id}`,
+  });
+
+  log.debug(`Password reset email sent to ${email}`);
+  return res.send(message);
 }
