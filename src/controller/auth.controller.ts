0d87cb2a4e8defaa84b8fbdf84a06c1ea7c751d8
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-20 20:01:38 +0400

    add: refresh access token handler

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
 import { CreateSessionInput } from "../schema/auth.schema";
-import { findUserByEmail } from "../service/user.service";
-import { signAccessToken, signRefreshToken } from "../service/auth.service";
+import { findUserByEmail, findUserById } from "../service/user.service";
+import {
+  findSessionById,
+  signAccessToken,
+  signRefreshToken,
+} from "../service/auth.service";
+import { get } from "lodash";
+import { verifyJwt } from "../utils/jwt";
 
 export async function createSessionHandler(
   req: Request<{}, {}, CreateSessionInput>,
@@ -37,4 +43,33 @@ export async function createSessionHandler(
     accessToken,
     refreshToken,
   });
+}
+
+export async function refreshAccessTokenHandler(req: Request, res: Response) {
+  const refreshToken = get(req, "headers.x-refresh") as string;
+
+  const decoded = verifyJwt<{ session: string }>(
+    refreshToken,
+    "refreshTokenPublicKey"
+  );
+
+  if (!decoded) {
+    return res.status(401).send("Could not refresh access token");
+  }
+
+  const session = await findSessionById(decoded.session);
+
+  if (!session || !session.valid) {
+    return res.status(401).send("Could not refresh access token");
+  }
+
+  const user = await findUserById(String(session.user));
+
+  if (!user) {
+    return res.status(401).send("Could not refresh access token");
+  }
+
+  const accessToken = signAccessToken(user);
+
+  return res.send({ accessToken });
 }
