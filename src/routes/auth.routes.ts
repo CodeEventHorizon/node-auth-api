5cf7afc1f1679e8ffb1ff58f92b443eb11334b5a
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-20 19:26:07 +0400

    add: create session route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import validateResource from "../middleware/validateResource";
+import { createSessionSchema } from "../schema/auth.schema";
+import { createSessionHandler } from "../controller/auth.controller";
 
 const router = express.Router();
+
+router.post(
+  "/api/sessions",
+  validateResource(createSessionSchema),
+  createSessionHandler
+);
 
 export default router;
