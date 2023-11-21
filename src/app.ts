e3cc6198fe545db65cc413adae155591ea08d158
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-11-21 16:17:05 +0400

    chore: app.js comments

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,34 @@
+// Modules
 import express from "express";
 import config from "config";
-import connectToDb from "./utils/connectToDb";
-import log from "./utils/logger";
+
+// Routes
 import router from "./routes";
+
+// Middleware
 import deserializeUser from "./middleware/deserializeUser";
 
+// Utils
+import connectToDb from "./utils/connectToDb";
+import log from "./utils/logger";
+
+// Variables
 const app = express();
-
-app.use(express.json());
-
-app.use(deserializeUser);
-
-app.use(router);
-
 const port = config.get("port");
 
+// 1. Body parser
+app.use(express.json());
+
+// 2. Check for token
+app.use(deserializeUser);
+
+// 3. Routes
+app.use(router);
+
+// 4. Listen app
 app.listen(port, () => {
   log.info(`App started at http:localhost:${port}`);
 
+  // 5. Connect to DB
   connectToDb();
 });
